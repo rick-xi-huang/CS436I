a479e57930b68235808ab8ec0def91051590a9ac
hkhx417 <hkhx417@students.cs.ubc.ca>
2020-06-30 23:33:37 -0700

    Completed a3

---
+++ a2/backend/routes/messages.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb://localhost:27017/myapp";
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
